948fa8418589a93a72232e2aa286a3b8d0b8c6a7
Filip <stefaniak@gmail.com>
2021-11-26 13:06:15 +0100

    new lib

--- 1-gpx/data2csv.sh
+++ 1-gpx/data2csv.sh
@@ -5,5 +5,5 @@ do
     echo $f
     baza=$(basename $f .tcx | sed -e 's/Koko_Nut_/polar/g' -e 's/activity_786315/8291/g')
     #gpsbabel -t -i gtrnctr -f $f -x track,speed -o unicsv -F $baza.csv
-    ./tcx_parser2.py --tcx $f > $baza.csv
+    ./tcx_parser2.py --tcx $f --csv $baza.csv
 done
